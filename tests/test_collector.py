6f654941d3f4eedf7246f6c15b369a03f3553c13
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-07-26 11:23:36 +1000

    Fixed the tests

--- tests/test_collector.py
+++ tests/test_collector.py
@@ -8,7 +8,7 @@ class Arithmetic(TestCase):
         x=int_(min=0), y=int_(min=0)
     ):
         the_sum = x + y
-        assert the_sum >= x and the_sum < y
+        assert the_sum >= x and the_sum >= y
 
     @qc
     def dumb():
