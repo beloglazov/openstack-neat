b7608300ab045cae3f126099dd5f223cb1fe357c
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-08-16 16:10:05 +1000

    Split db.py into two modules to separate the Database class

--- neat/db.py
+++ neat/db.py
@@ -13,54 +13,19 @@
 # limitations under the License.
 
 from contracts import contract
-from neat.contracts_extra import *
-
 from sqlalchemy import *
-
-
-@contract
-def init_db(sql_connection):
-    """ Initialize the database.
-
-    :param sql_connection: A database connection URL.
-     :type sql_connection: str
-
-    :return: The initialized database.
-     :rtype: Database
-    """
-    engine = create_engine(sql_connection)  # 'sqlite:///:memory:'
-    metadata = MetaData()
-    metadata.bind = engine
-
-    vms = Table('vms', metadata,
-                Column('id', Integer, primary_key=True),
-                Column('uuid', String(36), nullable=False))
-
-    vm_resource_usage = Table('vm_resource_usage', metadata,
-                Column('id', Integer, primary_key=True),
-                Column('vm_id', Integer),
-                Column('timestamp', Time, nullable=False),
-                Column('cpu_mhz', Integer, nullable=False))
-
-    metadata.create_all(engine)
-    engine.connect()
-
-    return Database(vms, vm_resource_usage)
 
 
 class Database(object):
     """ A class representing the database, where fields are tables.
     """
 
-    @contract
+    @contract(vms=Table, vm_resource_usage=Table)
     def __init__(self, vms, vm_resource_usage):
         """ Initialize the database.
 
         :param vms: The vms table.
-         :type vms: Table
-
         :param vm_resource_usage: The vm_resource_usage table.
-         :type vm_resource_usage: Table
         """
         self._vms = vms
         self._vm_resource_usage = vm_resource_usage
---
+++ neat/db_utils.py
@@ -0,0 +1,50 @@
+# Copyright 2012 Anton Beloglazov
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from contracts import contract
+from neat.contracts_extra import *
+
+from sqlalchemy import *
+
+from neat.db import Database
+
+
+@contract
+def init_db(sql_connection):
+    """ Initialize the database.
+
+    :param sql_connection: A database connection URL.
+     :type sql_connection: str
+
+    :return: The initialized database.
+     :rtype: Database
+    """
+    engine = create_engine(sql_connection)  # 'sqlite:///:memory:'
+    metadata = MetaData()
+    metadata.bind = engine
+
+    vms = Table('vms', metadata,
+                Column('id', Integer, primary_key=True),
+                Column('uuid', String(36), nullable=False))
+
+    vm_resource_usage = Table('vm_resource_usage', metadata,
+                Column('id', Integer, primary_key=True),
+                Column('vm_id', Integer, ForeignKey('vms.id'), nullable=False),
+                Column('timestamp', DateTime, server_default=text("sysdate")),
+                Column('cpu_mhz', Integer, nullable=False))
+
+    metadata.create_all()
+    engine.connect()
+
+    return Database(vms, vm_resource_usage)
