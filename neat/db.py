c11690e3c1aae330116f736a6c25c5fc658e0bf3
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-08-17 15:41:49 +1000

    Added a connection field to Database, moved some tests to test_db.py

--- neat/db.py
+++ neat/db.py
@@ -14,18 +14,43 @@
 
 from contracts import contract
 from sqlalchemy import *
+from sqlalchemy.engine.base import Connection
 
 
 class Database(object):
     """ A class representing the database, where fields are tables.
     """
 
-    @contract(vms=Table, vm_resource_usage=Table)
-    def __init__(self, vms, vm_resource_usage):
+    @contract(connection=Connection, vms=Table, vm_resource_usage=Table)
+    def __init__(self, connection, vms, vm_resource_usage):
         """ Initialize the database.
 
+        :param connection: A database connection table.
         :param vms: The vms table.
         :param vm_resource_usage: The vm_resource_usage table.
         """
+        self.connection = connection
         self.vms = vms
         self.vm_resource_usage = vm_resource_usage
+
+    @contract
+    def select_cpu_mhz_for_vm(self, uuid, n):
+        """ Select n last values of CPU MHz for a VM UUID.
+
+        :param uuid: The UUID of a VM.
+         :type uuid: str[36]
+
+        :param n: The number of last values to select.
+         :type n: int,>0
+
+        :return: The list of n last CPU Mhz values.
+         :rtype: list(int)
+        """
+        sel = select([self.vm_resource_usage.c.cpu_mhz]). \
+            where(and_(
+                self.vms.c.id == self.vm_resource_usage.c.vm_id,
+                self.vms.c.uuid == uuid)). \
+            order_by(self.vm_resource_usage.c.id.desc()). \
+            limit(n)
+        res = self.connection.execute(sel).fetchall()
+        return [x[0] for x in res]
---
+++ tests/test_db.py
@@ -0,0 +1,46 @@
+# Copyright 2012 Anton Beloglazov
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from mocktest import *
+from pyqcy import *
+
+from sqlalchemy import *
+
+import neat.db_utils as db_utils
+
+
+class DbUtils(TestCase):
+
+    @qc(1)
+    def insert_select():
+        db = db_utils.init_db('sqlite:///:memory:')
+        db.vms.insert().execute(uuid='test')
+        assert db.vms.select().execute().first()['uuid'] == 'test'
+        db.vm_resource_usage.insert().execute(vm_id=1, cpu_mhz=1000)
+        assert db.vm_resource_usage.select().execute().first()['cpu_mhz'] == 1000
+
+    @qc()
+    def select_cpu_mhz_for_vm(
+        uuid=str_(of='abc123-', min_length=36, max_length=36),
+        cpu_mhz=list_(of=int_(min=0, max=3000), min_length=0, max_length=10),
+        n=int_(min=1, max=10)
+    ):
+        db = db_utils.init_db('sqlite:///:memory:')
+        result = db.vms.insert().execute(uuid=uuid)
+        vm_id = result.inserted_primary_key[0]
+        for mhz in reversed(cpu_mhz):
+            db.vm_resource_usage.insert().execute(
+                vm_id=vm_id,
+                cpu_mhz=mhz)
+        assert db.select_cpu_mhz_for_vm(uuid, n) == cpu_mhz[:n]
