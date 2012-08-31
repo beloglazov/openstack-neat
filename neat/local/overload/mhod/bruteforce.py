68f90a0d5c2c1b59ec5c6efd2d48cfa22616b230
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-08-31 12:17:26 +1000

    Added solve2 to bruteforce

--- neat/local/overload/mhod/bruteforce.py
+++ neat/local/overload/mhod/bruteforce.py
@@ -12,8 +12,44 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-""" Function for solving NLP problems using the bruteforce method.
+""" Functions for solving NLP problems using the bruteforce method.
 """
 
 from contracts import contract
 from neat.contracts_extra import *
+
+from neat.common import frange
+
+
+@contract
+def solve2(objective, constraint, step, limit):
+    """ Solve a maximization problem for 2 states.
+
+    :param objective: The objective function.
+     :type objective: function
+
+    :param constraint: A tuple representing the constraint.
+     :type constraint: tuple(function, function, number)
+
+    :param step: The step size.
+     :type step: number,>0
+
+    :param limit: The maximum value of the variables.
+     :type limit: number,>0
+
+    :return: The problem solution.
+     :rtype: list(number)
+    """
+    res_best = 0
+    solution = []
+    for x in frange(0, limit, step):
+        for y in frange(0, limit, step):
+            try:
+                res = objective(x, y)
+                if res > res_best and \
+                   constraint[1](constraint[0](x, y), constraint[2]):
+                    res_best = res
+                    solution = [x, y]
+            except ZeroDivisionError:
+                pass
+    return solution
--- tests/local/overload/mhod/test_bruteforce.py
+++ tests/local/overload/mhod/test_bruteforce.py
@@ -15,8 +15,33 @@
 from mocktest import *
 from pyqcy import *
 
+from operator import le
+
 import neat.local.overload.mhod.bruteforce as b
 
 
 class Bruteforce(TestCase):
-    pass
+
+    def test_solve2(self):
+        def fn1(x, y):
+            return x + y
+
+        def fn2(x, y):
+            return 2 * x + y
+
+        def fn3(x, y):
+            return x - y
+
+        def fn4(x, y):
+            return x / y
+
+        self.assertEqual([round(x, 1) for x in b.solve2(fn1, (fn1, le, 10), 0.1, 1.0)],
+                         [1.0, 1.0])
+        self.assertEqual([round(x, 1) for x in b.solve2(fn1, (fn1, le, 0.5), 0.1, 1.0)],
+                         [0.0, 0.5])
+        self.assertEqual([round(x, 1) for x in b.solve2(fn2, (fn1, le, 0.5), 0.1, 1.0)],
+                         [0.5, 0.0])
+        self.assertEqual([round(x, 1) for x in b.solve2(fn3, (fn3, le, 10), 0.1, 1.0)],
+                         [1.0, 0.0])
+        self.assertEqual([round(x, 1) for x in b.solve2(fn4, (fn4, le, 10), 0.1, 1.0)],
+                         [1.0, 0.1])
