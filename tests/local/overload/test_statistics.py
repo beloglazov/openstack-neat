8b5da99281b0b8b147823d745ce8d01bad9d1a60
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-09-14 16:23:10 +1000

    Stared working on statistics based overload detection algorithms

---
+++ tests/local/overload/test_statistics.py
@@ -0,0 +1,45 @@
+# Copyright 2012 Anton Beloglazov
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from mocktest import *
+from pyqcy import *
+
+import neat.local.overload.statistics as stats
+
+
+class Statistics(TestCase):
+
+    pass
+
+    # @qc(10)
+    # def overloading_steps(
+    #     time_step=int_(min=0, max=10),
+    #     migration_time=int_(min=0, max=10),
+    #     utilization=list_(of=float)
+    # ):
+    #     alg = trivial.no_migrations_factory(time_step, migration_time, {'threshold': 0.5})
+    #     assert alg(utilization) == (False, {})
+
+    # def test_threshold(self):
+    #     self.assertTrue(trivial.threshold(0.5, [0.9, 0.8, 1.1, 1.2, 1.3]))
+    #     self.assertTrue(trivial.threshold(0.5, [0.9, 0.8, 1.1, 1.2, 0.6]))
+    #     self.assertFalse(trivial.threshold(0.5, [0.9, 0.8, 1.1, 1.2, 0.5]))
+    #     self.assertFalse(trivial.threshold(0.5, [0.9, 0.8, 1.1, 1.2, 0.3]))
+
+    # def test_threshold_factory(self):
+    #     alg = trivial.threshold_factory(300, 20, {'threshold': 0.5})
+    #     self.assertEquals(alg([0.9, 0.8, 1.1, 1.2, 1.3]), (True, {}))
+    #     self.assertEquals(alg([0.9, 0.8, 1.1, 1.2, 0.6]), (True, {}))
+    #     self.assertEquals(alg([0.9, 0.8, 1.1, 1.2, 0.5]), (False, {}))
+    #     self.assertEquals(alg([0.9, 0.8, 1.1, 1.2, 0.3]), (False, {}))
