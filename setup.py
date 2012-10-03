2f6f5ef16a667eae66bb9216b52bfd8178189e96
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-10-03 17:40:48 +1000

    Added a manifest, fixed bdist_rpm

--- setup.py
+++ setup.py
@@ -15,6 +15,53 @@
 """
 The OpenStack Neat Project
 ==========================
+
+OpenStack Neat is a project intended to provide an extension to
+OpenStack implementing dynamic consolidation of Virtual Machines (VMs)
+using live migration. The major objective of dynamic VM consolidation
+is to improve the utilization of physical resources and reduce energy
+consumption by re-allocating VMs using live migration according to
+their real-time resource demand and switching idle hosts to the sleep
+mode. Apart from consolidating VMs, the system should be able to react
+to increases in the resource demand and deconsolidate VMs when
+necessary to avoid performance degradation. In general, the problem of
+dynamic VM consolidation includes 4 sub-problems: host underload /
+overload detection, VM selection, and VM placement.
+
+This work is conducted within the Cloud Computing and Distributed
+Systems (CLOUDS) Laboratory (http://www.cloudbus.org/) at the
+University of Melbourne. The problem of dynamic VM consolidation
+considering Quality of Service (QoS) constraints has been studied from
+the theoretical perspective and algorithms addressing the sub-problems
+listed above have been proposed [1], [2]. The algorithms have been
+evaluated using CloudSim (http://code.google.com/p/cloudsim/) and
+real-world workload traces collected from more than a thousand
+PlanetLab VMs hosted on servers located in more than 500 places around
+the world.
+
+The aim of the OpenStack Neat project is to provide an extensible
+framework for dynamic consolidation of VMs based on the OpenStack
+platform. The framework should provide an infrastructure enabling the
+interaction of components implementing the decision-making algorithms.
+The framework should allow configuration-driven switching of different
+implementations of the decision-making algorithms. The implementation
+of the framework will include the algorithms proposed in our previous
+works [1], [2].
+
+[1] Anton Beloglazov and Rajkumar Buyya, "Optimal Online Deterministic
+Algorithms and Adaptive Heuristics for Energy and Performance
+Efficient Dynamic Consolidation of Virtual Machines in Cloud Data
+Centers", Concurrency and Computation: Practice and Experience (CCPE),
+Volume 24, Issue 13, Pages: 1397-1420, John Wiley & Sons, Ltd, New
+York, USA, 2012. Download:
+http://beloglazov.info/papers/2012-optimal-algorithms-ccpe.pdf
+
+[2] Anton Beloglazov and Rajkumar Buyya, "Managing Overloaded Hosts
+for Dynamic Consolidation of Virtual Machines in Cloud Data Centers
+Under Quality of Service Constraints", IEEE Transactions on Parallel
+and Distributed Systems (TPDS), IEEE CS Press, USA, 2012 (in press,
+accepted on August 2, 2012). Download:
+http://beloglazov.info/papers/2012-host-overload-detection-tpds.pdf
 """
 
 import distribute_setup
@@ -32,7 +79,8 @@ setup(
     author_email='anton.beloglazov@gmail.com',
     url='https://github.com/beloglazov/openstack-neat',
     platforms='any',
-
+    include_package_data=True,
+    license='LICENSE',
     packages=find_packages(),
     test_suite='tests',
     tests_require=['pyqcy', 'mocktest', 'PyContracts'],
