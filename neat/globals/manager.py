acf6891dfb6729ad8d590e7ee7cdc62882f0da66
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-10-12 11:48:49 +1100

    Another minor fix

--- neat/globals/manager.py
+++ neat/globals/manager.py
@@ -331,7 +331,7 @@ def execute_underload(config, state, host):
 
     for vm in vms_cpu.keys():
         if not vm in vms_ram:
-            del vms_cpu[i]
+            del vms_cpu[vm]
 
     time_step = int(config['data_collector_interval'])
     migration_time = common.calculate_migration_time(
