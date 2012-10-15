55254c2fdc411c4e58af11ff61f4e0146f2eea13
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-10-15 14:47:13 +1100

    Added a script for installing OpenStack Neat in chkconfig

---
+++ setup/update-chkconfig.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+chkconfig --add openstack-neat-data-collector
+chkconfig --add openstack-neat-db-cleaner
+chkconfig --add openstack-neat-global-manager
+chkconfig --add openstack-neat-local-manager
