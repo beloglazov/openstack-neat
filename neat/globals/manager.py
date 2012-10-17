c7f1adbca0b0fb27ac0f7530a2cdaae90e409533
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-10-18 10:23:25 +1100

    Removed a comment

--- neat/globals/manager.py
+++ neat/globals/manager.py
@@ -1,4 +1,4 @@
-s# Copyright 2012 Anton Beloglazov
+# Copyright 2012 Anton Beloglazov
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
