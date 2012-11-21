265c6420ce13d24af598f3a4313ac403faa85fe5
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-11-21 19:13:20 +1100

    Added db-cleaner to the all-start scripts

--- all-start.sh
+++ all-start.sh
@@ -2,7 +2,7 @@
 
 ./compute-data-collector-start.py
 service openstack-neat-global-manager start
-service openstack-neat-global-manager start
+service openstack-neat-db-cleaner start
 
 sleep 2
 
--- all-stop.sh
+++ all-stop.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 service openstack-neat-global-manager stop
-service openstack-neat-global-manager stop
+service openstack-neat-db-cleaner stop
 ./compute-local-manager-stop.py
 ./compute-data-collector-stop.py
